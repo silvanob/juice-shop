18c464a278626f64045c9062aee03c760d480937
Silvano Biemans <silvano.biemans@hva.nl>
2021-05-12 14:38:14 +0200

    Update test for scoreboard
    Signed-off-by: Silvano Biemans <silvano.biemans@hva.nl>

--- frontend/src/app/Services/ransomware.service.spec.ts
+++ frontend/src/app/Services/ransomware.service.spec.ts
@@ -1,16 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 import { RansomwareService } from './ransomware.service';
 
 describe('RansomwareService', () => {
-  let service: RansomwareService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RansomwareService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [RansomwareService]
+    })
   });
 
-  it('should be created', () => {
+  it('should be created', inject([RansomwareService], (service: RansomwareService) => {
     expect(service).toBeTruthy();
-  });
+  }));
+
+  it('should send the decryption key and return', inject([RansomwareService, HttpTestingController],
+    fakeAsync((service: RansomwareService, httpMock: HttpTestingController) => {
+    let res: any
+    service.decrypt(null).subscribe((data) => (res = data))
+
+    const req = httpMock.expectOne('http://localhost:3000/rest/ransomware/decrypt')
+    req.flush({ data: 'hello' })
+    tick()
+
+    expect(req.request.method).toBe('POST')
+    expect(res).toEqual({ data: 'hello' })
+    httpMock.verify()
+  })
+  ));
 });
